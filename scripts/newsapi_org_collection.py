54748caab4133ea2b65bf43cfa0467db1ada85b2
letaoli123 <ericletao@yahoo.ca>
2025-11-14 15:24:01 -0500

    working collection of newsapi.org

--- scripts/newsapi_org_collection.py
+++ scripts/newsapi_org_collection.py
@@ -4,35 +4,31 @@ import os
 import urllib.parse
 from pathlib import Path
 
+from newsapi import NewsApiClient
+
 from dotenv import load_dotenv
+
+load_dotenv()
+NEWS_API_ORG_KEY = os.getenv('NEWSAPI_ORG_KEY')
+
 
 rootpath = Path(__file__).parent.parent
 data_path = rootpath / "data"
 
 collection_filepath = data_path / "mamdani_newsapi_org.json"
-load_dotenv()
-news_api_org_key = os.getenv('NEWSAPI_ORG_KEY')
-conn = http.client.HTTPSConnection('newsapi.org')
-
-params = urllib.parse.urlencode({
-    'apiKey': news_api_org_key,
-    'q': 'Zohran Mamdani',
-    'from': '2024-11-01',
-    'to': '2025-11-01',
-    'sortBy': 'publishedAt',
-    'language': 'en'
-})
-
-# GET request
-conn.request('GET', '/v2/everything?{}'.format(params))
-res = conn.getresponse()
-data = res.read()
+news_api = NewsApiClient(NEWS_API_ORG_KEY)
+all_articles = news_api.get_everything(
+    q='Zohran Mamdani',
+    from_param='2025-10-14',
+    to='2025-01-11',
+    language='en',
+    sort_by='relevancy',
+    page=2
+    )
 
 # Decode and parse JSON
-news_data = json.loads(data.decode('utf-8'))
-
 # Dump in JSON file
 with open(collection_filepath, 'a', encoding='utf-8') as f:
-    json.dump(news_data, f, ensure_ascii=False, indent=4)
+    json.dump(all_articles, f, ensure_ascii=False, indent=4)
 
 # print(f"Saved {len(news_data.ge)}")
